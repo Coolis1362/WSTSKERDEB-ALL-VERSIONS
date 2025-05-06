fd484e1ca7cd80954913a558d441c253107c44de
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-05-06 18:08:33 -0400

    Implement code changes to enhance functionality and improve performance

--- SOURCE_CODE/1.x.x/1.0.x/1.0.2/root/boot/ts-distro-distro_name.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-python3 ts-distro-distro_name.py
-read -s
---
+++ SOURCE_CODE/1.x.x/1.0.x/1.0.2/root/boot/wstskerdeb.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal EnableDelayedExpansion
+SET _INTERPOLATION_0=
+FOR /f "delims=" %%a in ('dirname "$0"') DO (SET "_INTERPOLATION_0=!_INTERPOLATION_0! %%a")
+cd "!_INTERPOLATION_0:~1!"
+python "wstskerdeb.py"
+pause
